8586a0034469b52ddc1227aab746f547a3d03dd7
Daniel Carriillo <dexter4873@gmail.com>
2022-11-28 21:03:51 -0600

    SPP with PSO solved

--- graph.py
+++ graph.py
@@ -4,11 +4,16 @@ import pso
 class Graph:
     """Class to represent a graph with an adjacency matrix"""
 
-    def __init__(self, n=0):
+    def __init__(self, n=0, matrix=None):
         """
         Initialize graph with n nodes.
         """
+        if matrix is None:
+            matrix = []
         self.matrix = [[-1 for _ in range(n)] for _ in range(n)]
+        for i in range(6):
+            for j in range(6):
+                self.matrix[i][j] = matrix[i][j]
 
     def __str__(self):
         """
@@ -56,16 +61,32 @@ class Graph:
         tn = origin  # Terminal node, last node selected to be part of the path
         n_max = len(self.matrix)  # Maximum number of node to be part of the path and the highest node ID
         k = 0  # Iteration variable
-        m = 4  # Heuristic operator
+        m = 2  # Heuristic operator
 
         # Main loop
-        while k < n_max and tn != n_max:
+        while k <= n_max and tn != n_max:
 
-            # Select node from linked nodes
+            # Select node from linked nodes to terminal node
             posible_nodes = [node for node, weight in enumerate(self.matrix[tn]) if weight > -1]
+            # Sorting posibles nodes list by priority in priority vector
+            posible_nodes.sort(key=lambda node: pv[node], reverse=True)
+            # Select best next node
             for node in posible_nodes:
-                pass
+                if (node - tn) > -m:
+                    path.append(node)
+                    tn = node
+                    pv[node] = float('-inf')
+                    break
+            # Check if path is complete
+            if path[-1] == destination:
+                break
             k += 1
+
+        # Check if path is valid
+        if path[-1] != destination:
+            path = []
+
+        return path
 
     def shortest_path(self, origin, destination):
         """
@@ -74,7 +95,14 @@ class Graph:
 
         # Fitness function
         def sp(priority_vector):
-            return sum(priority_vector)
+            path = self.create_path(origin, destination, priority_vector)
+            cost = 0
+            if path:
+                for i in range(1, len(path)):
+                    cost += self.matrix[path[i]][path[i - 1]]
+            else:
+                cost = float('inf')
+            return cost
 
         # Problem definition for PSO
         problem = {
@@ -84,5 +112,5 @@ class Graph:
             'VarMax': 1000,
         }
 
-        g_best, pop = pso.PSO(problem, MaxIter=1000, PopSize=50, c1=1.5, c2=2, w=1, wdamp=0.995)
-        return g_best
+        g_best, pop = pso.PSO(problem, MaxIter=500, PopSize=30, c1=1.5, c2=2, w=1, wdamp=0.995)
+        return self.create_path(origin, destination, g_best['position'].tolist())
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from graph import Graph
 
 if __name__ == '__main__':
-    graph = Graph(5)
-    b = graph.shortest_path(1, 3)
-    print(b)
+
+    matrix = [[-1, 4, 8, -1, -1, -1],
+              [4, -1, 5, 3, 1, -1],
+              [8, 5, -1, -1, 9, -1],
+              [-1, 3, -1, -1, 4, 2],
+              [-1, 1, 9, 4, -1, 6],
+              [-1, -1, -1, 2, 6, -1]]
+
+    graph = Graph(6, matrix)
+
+    print(graph.shortest_path(0, 5))
+
